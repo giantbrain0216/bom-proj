1ba026d3a3573adae8e6d0f0cfc4085709489176
Prayatna <prayatna.bhattarai@gmail.com>
2018-04-14 11:06:54 +1000

    controller uses traits

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,51 +3,29 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use GuzzleHttp;
-use Khill\Lavacharts\Lavacharts;
+use App\Http\Traits\RequestTrait;
 
 class dashboardController extends Controller
 {
     //
+    use RequestTrait;
 
     public function index()
     {
-        $client = new GuzzleHttp\Client();
-        $res = $client->request('GET', 'http://reg.bom.gov.au/fwo/IDN60903/IDN60903.94926.json');
 
-        $all_data = json_decode($res->getBody(), true);
+        $cbr = "http://reg.bom.gov.au/fwo/IDN60903/IDN60903.94926.json";
 
-        $temperature= array();
-        $date = array();
+        $all_data = $this->getWeatherData($cbr);
+        $currentTemperature = $this->getCurrentTemperature($all_data);
+        $currentWind = $this->getCurrentWind($all_data);
+        $currentWindDirection = $this->getCurrentWindDirection($all_data);
+        $currentRelativeHumidity = $this->getCurrentRelativeHumidity($all_data);
 
-        $currentTemperature = $all_data["observations"]["data"][0]["air_temp"];
-        $currentWind = $all_data["observations"]["data"][0]["wind_spd_kmh"];
-        $currentWindDirection = $all_data["observations"]["data"][0]["wind_dir"];
-        $currentRelativeHumidity = $all_data["observations"]["data"][0]["rel_hum"];
+        $lava = $this->createChart($all_data);
 
-        for($i = 0; $i < 100; $i++){
-            $temperature[] = round($all_data["observations"]["data"][$i]["air_temp"],1);
-            $date[] = $all_data["observations"]["data"][$i]["local_date_time_full"];
-        }
-//        print_r($date);
-
-        $lava = new Lavacharts;
-        $weather = $lava->DataTable();
-        $weather->addDateTimeColumn(' Date')
-            ->addNumberColumn('Temperature');
-        for($i = 0; $i < 100; $i++){
-            $weather->addRow([$date[$i],$temperature[$i]]);
-
-        }
-
-        $lava->LineChart('Temps', $weather, [
-            'title' => 'Temperature for every 30 minutes'
-        ]);
 
 
         return view('/dashboard')->with(array(
-            'temperature' => $temperature,
-            'date' => $date,
             'currentTemperature' =>$currentTemperature,
             'currentWind'=>$currentWind,
             'currentWindDirection'=>$currentWindDirection,
